039777f68e050202efd7ae07ebdf274a4dca7124
Jamal Farah <moulayjam@gmail.com>
2019-07-08 17:44:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,40 @@
-def find_element_index(array,value)
-  array.include?(value)
+def find_element_index(array, value_to_find)
+  array.index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  max_value = 0
+  array.each_with_index do |element, index|
+    if array[index] > array[index+1].to_i && array[index] > max_value
+      max_value = array[index]
+    elsif array[index] < array[index+1].to_i && array[index+1] > max_value
+      max_value = array[index+1]
+    end
+  end
+  max_value
 end
 
 def find_min_value(array)
-  # Add your solution here
+  min_value = ""
+  max_index = array.length - 1 
+  array.each_with_index do |element, index|
+    if index+1 <= max_index
+      if array[index] < array[index+1]
+        if (min_value.class == String && min_value.empty?)
+          min_value = array[index]
+        elsif (array[index] < min_value)
+          min_value = array[index]
+        end
+      elsif (array[index+1] < array[index])
+        if (min_value.class == String && min_value.empty?)
+          min_value = array[index+1]
+        elsif (array[index+1] < min_value)
+          min_value = array[index+1]
+        end
+      end
+    end
+  end
+  min_value
+  
 end
+
